51105a5d2c5e86efca71bad425feab7a1590b2fb
bikram ghimire <ikramghimire@gmail.com>
2021-05-16 21:08:35 +0545

    simple webdesign using html css and js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function myFunctionx() {
+  var x = document.querySelector(".nav ul li input");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunctiony() {
+  var y = document.querySelector(".nav-mobile");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
+}
